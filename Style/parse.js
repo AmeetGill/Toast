83317ed7aa3e7d2d9fab1c240a47bfc2f8c7a086
AmeetGill <aameetpal_be15@thapar.edu>
2019-08-22 00:05:18 +0530

    added json converter

---
+++ Style/parse.js
@@ -0,0 +1,18 @@
+let jsonObj = require('./style.json')
+
+for(let key in jsonObj){
+	let value = jsonObj[key]
+	for( let i = 0; i  < value.length; i++){
+		let char = value.charAt(i)
+		if(char == char.toUpperCase()){
+			let front = value.substring(0,i);
+			let tail = value.substring(i+1);
+			let middle = "-"+char.toLowerCase();
+			value = front+middle+tail
+			i += 1
+		}
+	}
+	jsonObj[key] = value
+}
+
+console.log(jsonObj)
