3d5ce9860fab3e38582ac92a878bdcf928ebfecf
Ameetpal Singh <ameetpal.singh@oracle.com>
2019-08-26 21:34:55 +0530

    bug fixes

--- Tags/CustomTag.py
+++ Tags/CustomTag.py
@@ -1,12 +1,14 @@
 from Tag import Tag
 class CustomTag(Tag):
     def __init__(self,customGenerator,*args):
-        self.custom_generator = customGenerator
+        self.custom_generator_func = customGenerator
+        self.custom_generator_args = args
 
     def custom_generator(self,*args):
-        tag_str_output = self.custom_generator(args)
+        tag_str_output = self.custom_generator_func(args)
         assert(tag_str_output is ""), "output of your {} custom generator should be a string".format("curtom Tag")
+        return tag_str_output
 
-
-
-
+    def generator(self):
+        if(self.custom_generator):
+            return self.custom_generator(self.args)
