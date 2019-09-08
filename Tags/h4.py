87219bffa3373ae164a90bfccde50fc53c871027
Ameetpal Singh <ameetpal.singh@oracle.com>
2019-09-08 16:18:24 +0530

    added header files

---
+++ Tags/h4.py
@@ -0,0 +1,6 @@
+from tag import tag
+class h4(tag):
+	def __init__(self ,tagId = "",className = "",style = {},childrens = [],tagType="div",inlineStyle={},text = ""):
+		super().__init__(tagId = tagId, className = className, style = style, childrens = childrens, tagType = "h4", inlineStyle = inlineStyle,text=text)
+		
+
